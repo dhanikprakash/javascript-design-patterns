31ffbc00d48256daa4c7a0e951624a279ac1d92f
dhanikprakash <dhanikprakash@gmail.com>
2021-02-21 14:30:41 +0000

    adding more  patterns

---
+++ facade-pattern/index.js
@@ -0,0 +1,29 @@
+function getUsers() {
+  return getFetch("https://jsonplaceholder.typicode.com/users");
+}
+
+function getUserPosts(userId) {
+  return getFetch("https://jsonplaceholder.typicode.com/posts", {
+    userId: userId,
+  });
+}
+
+getUsers().then((users) => {
+  users.forEach((user) => {
+    getUserPosts(user.id).then((posts) => {
+      console.log(user.name);
+      console.log(posts.length);
+    });
+  });
+});
+
+
+function getFetch(url, params = {}) {
+  return axios({
+    url: url,
+    method: "GET",
+    params: params,
+  }).then((res) => res.data);
+}
+getUsers();
+getUsers(2);
